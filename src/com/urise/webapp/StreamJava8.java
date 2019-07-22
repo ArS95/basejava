59561371971e196b4f260be809f7313d4e7e8b83
Arsen Temirtasov <temirtasov95@gmail.com>
2019-07-23 00:10:13 +0600

    hw 12 last fix

--- src/com/urise/webapp/StreamJava8.java
+++ src/com/urise/webapp/StreamJava8.java
@@ -3,14 +3,12 @@ package com.urise.webapp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StreamJava8 {
     public static void main(String[] args) {
         System.out.println(minValue(new int[]{8, 9, 7, 4, 1, 2, 3}));
-        System.out.println(oddOrEven(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9))));
+        System.out.println(oddOrEven(new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9))));
     }
 
     private static int minValue(int[] values) {
@@ -18,11 +16,11 @@ public class StreamJava8 {
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        Supplier<Stream<Integer>> streamSupplier = integers::stream;
-        return getList(streamSupplier, streamSupplier.get().mapToInt(Integer::intValue).sum() % 2 != 0);
-    }
-
-    private static List<Integer> getList(Supplier<Stream<Integer>> streamSupplier, boolean sumResult) {
-        return streamSupplier.get().filter(a -> (a % 2 == 0) == sumResult).collect(Collectors.toList());
+        int x = integers.stream()
+                .mapToInt(Integer::intValue)
+                .sum() % 2;
+        return integers.stream()
+                .filter(a -> a % 2 != x)
+                .collect(Collectors.toList());
     }
 }
